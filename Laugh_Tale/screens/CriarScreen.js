79e7d41c2e05040316f8e94d33c8200b4fb7be19
eduardomlucas <eduardomachadolucas1@gmail.com>
2023-09-01 19:19:47 -0300

    feat: botão de descarte / deleting tests

--- Laugh_Tale/screens/CriarScreen.js
+++ Laugh_Tale/screens/CriarScreen.js
@@ -1,139 +1,47 @@
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/core'
-import { StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native'
-import { auth, firestore, firebase, storage} from '../firebase'
+import { Text, TouchableOpacity, View, KeyboardAvoidingView } from 'react-native'
+import { firestore} from '../firebase'
+import styles from '../estilo';
 
 const CriarScreen = () => {
     const navigation = useNavigation();
-    
-    const [imageUri, setImageUri] = useState(null);
-    const [nomeManga, setNomeManga] = useState('')
-    const [nomeAutor, setNomeAutor] = useState('') 
-    const [nrCapitulo, setNrCapitulo] = useState('')
-
-    const handleVoltar=()=>{
-        navigation.replace("Home")
-    }
-    
-    const salvarManga=()=>{
-        if(nomeManga === "" && nrCapitulo === ""){
-            return alert ("Insira o Nome do mangá e número do capítulo antes")
-        }
-        if(nomeManga === ""){
-            return alert('Insira o Nome do mangá antes')
-        }
-        if(nrCapitulo == ""){
-            return alert('Insira o número do capítulo antes')
-        }
-        console.log("salvando mangá")
-        navigation.replace("Teste")
-        firestore
-        .collection('manga')
-        .add({
-            nomeManga: nomeManga,
-            nomeManga: nomeAutor,
-            nrCapitulo: nrCapitulo
-        })
-        .then(()=>{
-            navigation.replace("Teste")
-            alert(nomeManga+'Capítulo: '+nrCapitulo+ '\nCadastado com sucesso!');
-        })
-    }
 
     return (
-      <View style={styles.container}>
-        <View style={styles.inputContainer} >
-        <TextInput
-          placeholder="Nome do Mangá / HQ"
-          value={nomeManga}
-          onChangeText={text => setNomeManga(text)}
-          style={styles.input}
-        />
-
-        <TextInput
-          placeholder="Autor"
-          value={nomeAutor}
-          onChangeText={text => setNomeAutor(text)}
-          style={styles.input}
-        />
-        
-        <TextInput
-          placeholder="Capítulo"
-          value={nrCapitulo}
-          onChangeText={text => setNrCapitulo(text)}
-          style={styles.input}
-        />
+      <KeyboardAvoidingView style={styles.container}>
+        <View style={styles.buttonContainerLeitura}>
+        <TouchableOpacity 
+            style={[styles.button, styles.buttonOutlineLeitura]}>
+          <Text style={styles.buttonOutlineText}>Cancelar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={[styles.button, styles.buttonOutlineLeitura]}>
+          <Text style={styles.buttonOutlineText}>Descartar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={[styles.button, styles.buttonOutlineLeitura]}>
+          <Text style={styles.buttonOutlineText}>Salvar</Text>
+          </TouchableOpacity>
         </View>
-        
-        <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}onPress={salvarManga}>
-          <Text style={styles.buttonText}>Iniciar Criação</Text>
-        </TouchableOpacity>
+        <View style={styles.boardContainer}>
 
-        <TouchableOpacity style={styles.buttonBack}onPress={handleVoltar}>
-          <Text style={styles.buttonText}>Voltar</Text>
-        </TouchableOpacity>
         </View>
-      </View>
+        <View style={styles.buttonContainerLeitura}>
+          <TouchableOpacity 
+            style={[styles.button, styles.buttonOutlineLeitura]}>
+          <Text style={styles.buttonOutlineText}>Persongem</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+              style={[styles.button, styles.buttonOutlineLeitura]}>
+            <Text style={styles.buttonOutlineText}>Falas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+              style={[styles.button, styles.buttonOutlineLeitura]}>
+            <Text style={styles.buttonOutlineText}>Painéis</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     )
   }
 
 export default CriarScreen;
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    inputContainer: {
-      width: '80%'
-    },
-    input: {
-      backgroundColor: 'white',
-      paddingHorizontal: 15,
-      paddingVertical: 10,
-      borderRadius: 10,
-      marginTop: 5,
-    },
-    buttonContainer: {
-      width: '60%',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 40,
-    },
-    button: {
-        backgroundColor: '#8F43EE',
-        width: '100%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-        marginTop: 30,
-      },
-    buttonOutline: {
-      backgroundColor: 'white',
-      borderColor: '#0782F9',
-      borderWidth: 2,
-      width: '100%',
-      marginBottom: 5
-    },
-    buttonBack: {
-        backgroundColor: '#2B2730',
-        width: '100%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-        marginTop: 5,
-      },
-    buttonText: {
-      color: 'white',
-      fontWeight: '700',
-      fontSize: 16,
-    },
-    buttonOutlineText: {
-      color: '#0782F9',
-      fontWeight: '700',
-      fontSize: 16,
-    },
-  })
-  
